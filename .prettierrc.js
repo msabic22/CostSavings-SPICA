6ba2a9ec9f75c5ddd066b5cb9ff04ecb94f74c93
Marino Sabic <msabic18@gmail.com>
2022-01-24 19:04:35 +0100

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'always',
+    trailingComma: 'all',
+    endOfLine: 'lf',
+    semi: false,
+    singleQuote: true,
+    printWidth: 140,
+    tabWidth: 4,
+}
---
+++ pages/generate-data.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap'
+
+const GenerateData = () => {
+    return (
+        <div>
+            <Container>
+                <h1 className="mt-5">Generate Data</h1>
+                <Row>
+                    <Col>
+                        <Card></Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default GenerateData
